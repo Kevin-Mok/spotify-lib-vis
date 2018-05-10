8efc4e3033a0f373c549d37c3a664346e01f9e0e
Chris <chrisshyi13@gmail.com>
2018-05-09 21:59:59 -0400

    Change name of project
    Name of project changed from 'musicdata' to 'musicvis'

--- musicvis/settings.py
+++ musicvis/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'musicdata.urls'
+ROOT_URLCONF = 'musicvis.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'musicdata.wsgi.application'
+WSGI_APPLICATION = 'musicvis.wsgi.application'
 
 
 # Database
--- musicvis/wsgi.py
+++ musicvis/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "musicdata.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "musicvis.settings")
 
 application = get_wsgi_application()
