797115e9cb07b7aa9dbfe48f2e750e93fd754ab0
Chris Shyi <chrisshyi13@gmail.com>
2018-06-08 18:45:16 -0400

    Clean up utils.py to use database
    Uses of the original library_stats dictionary in utils.py were either removed or commented out, replaced by the saving of models into the database.

--- spotifyvis/models.py
+++ spotifyvis/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 # id's are 22 in length in examples but set to 30 for buffer
-id_length=30
-
+MAX_ID = 30
 #  Artist {{{ # 
 
 class Artist(models.Model):
@@ -10,7 +9,7 @@ class Artist(models.Model):
         verbose_name = "Artist"
         verbose_name_plural = "Artists"
 
-    artist_id = models.CharField(primary_key=True, max_length=id_length)
+    artist_id = models.CharField(primary_key=True, max_length=MAX_ID)
     # unique since only storing one genre per artist right now
     name = models.CharField(unique=True, max_length=50)
     genre = models.CharField(max_length=20)
@@ -27,7 +26,7 @@ class User(models.Model):
         verbose_name = "User"
         verbose_name_plural = "Users"
 
-    user_id = models.CharField(primary_key=True, max_length=id_length) # the user's Spotify ID
+    user_id = models.CharField(primary_key=True, max_length=MAX_ID) # the user's Spotify ID
     #  username = models.CharField(max_length=30) # User's Spotify user name, if set
 
     def __str__(self):
@@ -43,7 +42,7 @@ class Track(models.Model):
         verbose_name = "Track"
         verbose_name_plural = "Tracks"
 
-    track_id = models.CharField(primary_key=True, max_length=id_length)
+    track_id = models.CharField(primary_key=True, max_length=MAX_ID)
     #  artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     artists = models.ManyToManyField(Artist, blank=True)
     year = models.PositiveSmallIntegerField()
@@ -79,3 +78,39 @@ class AudioFeatures(models.Model):
         return super(AudioFeatures, self).__str__()
 
 #  }}} AudioFeatures # 
+
+
+'''
+class UserAudioSummary(models.Model):
+    """
+    Stores the summary of a user's audio preferences
+    """ 
+    class Meta:
+        verbose_name = "AudioFeatures"
+        verbose_name_plural = "AudioFeatures"
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True,)
+    avg_acousticness = models.DecimalField(decimal_places=3, max_digits=3)
+    stdev_acousticness = models.DecimalField(decimal_places=3, max_digits=3)
+
+    avg_danceability = models.DecimalField(decimal_places=3, max_digits=3)
+    stdev_danceability = models.DecimalField(decimal_places=3, max_digits=3)
+
+    avg_energy = models.DecimalField(decimal_places=3, max_digits=3)
+    stdev_energy = models.DecimalField(decimal_places=3, max_digits=3)
+
+    avg_instrumentalness = models.DecimalField(decimal_places=3, max_digits=3)
+    stdev_instrumentalness = models.DecimalField(decimal_places=3, max_digits=3)
+
+    avg_loudness = models.DecimalField(decimal_places=3, max_digits=6)
+    stdev_loudness = models.DecimalField(decimal_places=3, max_digits=6)
+
+    avg_speechiness = models.DecimalField(decimal_places=3, max_digits=3)
+    stdev_speechiness = models.DecimalField(decimal_places=3, max_digits=3)
+
+    avg_tempo = models.DecimalField(decimal_places=3, max_digits=6)
+    stdev_tempo = models.DecimalField(decimal_places=3, max_digits=6)
+
+    avg_valence = models.DecimalField(decimal_places=3, max_digits=3)
+    stdev_valence = models.DecimalField(decimal_places=3, max_digits=3)
+'''
--- spotifyvis/static/spotifyvis/scripts/test_db.js
+++ spotifyvis/static/spotifyvis/scripts/test_db.js
@@ -1,10 +1,4 @@
-console.log("{{ user_id }}");
-artist_data = JSON.parse('{{ artist_data }}');
-artist_data.forEach(function(d) {
-	console.log(d.name, d.num_songs);
-});
-
-d3.json("{% url "get_artist_data" user_id %}", function(error, data) {
+d3.json("{% url "get_artist_data", user_id %}").then(function(error, data) {
 	data.forEach(function(d) {
 		console.log(d.name, d.num_songs);
 	});
