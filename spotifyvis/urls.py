f624414701d9499537c02f03fe6c0cffef2756d3
Chris Shyi <chris.shyi@mail.utoronto.ca>
2018-06-28 21:52:24 -0400

    Refactor audio features graph code
    Closes #44. Audio features graph code is now in an external .js file.
    Also closes #45.

--- spotifyvis/urls.py
+++ spotifyvis/urls.py
@@ -9,11 +9,11 @@ urlpatterns = [
     path('user_data', user_data, name='user_data'),
     path('admin_graphs', admin_graphs, name='admin_graphs'),
     path('api/user_artists/<str:user_secret>', get_artist_data, name='get_artist_data'),
-    path('artists/<str:user_secret>', artist_data, name='display_artist_graph'),
+    path('graphs/artists/<str:user_secret>', artist_data, name='display_artist_graph'),
     path('api/user_genres/<str:user_secret>', get_genre_data, name='get_genre_data'),
     path('graphs/genre/<str:user_secret>', display_genre_graph,
          name='display_genre_graph'),
-    path('audio_features/<str:user_secret>', audio_features, name='display_audio_features'),
+    path('graphs/audio_features/<str:user_secret>', audio_features, name='display_audio_features'),
     path('api/audio_features/<str:audio_feature>/<str:user_secret>',
          get_audio_feature_data, name='get_audio_feature_data'),
 ]
